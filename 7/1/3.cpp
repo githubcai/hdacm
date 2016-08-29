7f0eb20906d55821da42d711f587a96390754429
caixukun <2468085704@qq.com>
2016-08-29 08:05:59 +0800

    a

--- 7/1/3.cpp
+++ 7/1/3.cpp
@@ -1,5 +1,23 @@
 #include <cstdio>
 
 int main(){
+    int T;
+    scanf("%d", &T);
+    while(T--){
+        int N; scanf("%d", &N);
+        double x1, y1, x2, y2, x3, y3, area = 0.0, xt = 0.0, yt = 0.0;
+        scanf("%lf%lf%lf%lf", &x1, &y1, &x2, &y2);
+        for(int i = 2; i < N; ++i){
+            scanf("%lf%lf", &x3, &y3);
+            double ax, ay, s;
+            ax = x1 + x2 + x3;
+            ay = y1 + y2 + y3;
+            s = ((x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1)) / 2.0;
+            xt += ax * s, yt += ay * s, area += s;
+            x2 = x3, y2 = y3;
+        }
+        double  xr = xt / area / 3, yr = yt / area / 3;
+        printf("%.02lf %.02lf\n", xr, yr);
+    }
     return 0;
 }
